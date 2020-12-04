15698fea764237ad8fc7bb3c0874ca92efdfcf4d
gtl_amit. mangaraj <amit.mangaraj@thegatewaycorp.com>
2020-12-04 18:37:32 +0530

    Changes

--- models/post.js
+++ models/post.js
@@ -13,6 +13,21 @@ const postSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    likes:[
+        {
+            type:ObjectId,
+            ref:"User"
+        }
+    ],
+    comments:[
+        {
+            text:String,
+            postedBy:{
+                type:ObjectId,
+                ref:"User"
+            }
+        }
+    ],
     postedBy: {
         type: ObjectId, //Id of the user 
         ref: "User"
--- routes/post.js
+++ routes/post.js
@@ -2,11 +2,11 @@ const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
 const requireLogin = require('../middeleware/requireLogin')
-const { route } = require('./auth')
+const { route, post } = require('./auth')
 const Post = mongoose.model("Post")
 //this is comment
-router.get('/getallpost',requireLogin, (req, res) => {
-    Post.find().populate("postedBy", "_id name").then(posts => {
+router.get('/getallpost', requireLogin, (req, res) => {
+    Post.find().populate("postedBy", "_id name").populate("comments.postedBy","_id name").then(posts => {
         res.json({ posts })
     }).catch(err => {
         console.log(err)
@@ -21,17 +21,9 @@ router.get('/mypost', requireLogin, (req, res) => {
     })
 })
 
-router.delete('/deletemypost', requireLogin, (req, res) => {
-
-    Post.findById({ _id }).then(myposts => {
-        res.status(200).json({ message: "Post deleted Succesfully" })
-    }).catch(err => {
-        console.log(err)
-    })
-})
 router.post('/createpost', requireLogin, (req, res) => {
     const { title, body, pic } = req.body
-    if (!title || !body|| !pic) {
+    if (!title || !body || !pic) {
         return res.status(422).json({ error: "Please add Title to post" })
     }
 
@@ -39,7 +31,7 @@ router.post('/createpost', requireLogin, (req, res) => {
     const post = new Post({
         title,
         body,
-        photo:pic,
+        photo: pic,
         postedBy: req.user
     })
     post.save().then(result => {
@@ -49,4 +41,73 @@ router.post('/createpost', requireLogin, (req, res) => {
             console.log(err)
         })
 })
+
+router.put('/like', requireLogin, (req, res) => {
+    Post.findByIdAndUpdate(req.body.postId,
+        {
+            $push: { likes: req.user._id } 
+        }, {
+        new: true
+    }).exec((err, result) => {
+        console.log(result)
+        if (err) {
+            return res.status(422).json({ error: err })
+        }
+        else {
+            res.json(result )
+        }
+    })
+})
+
+router.put('/unlike', requireLogin, (req, res) => {
+    Post.findByIdAndUpdate(req.body.postId,
+         {
+              $pull: { likes: req.user._id } 
+            }, {
+        new: true
+    }).exec((err, result) => {
+        if (err) {
+            return res.status(422).json({ error: err })
+        }
+        else {
+            res.json( result )
+        }
+    })
+})
+
+router.put('/comment', requireLogin, (req, res) => {
+    const comment = {text:req.body.text,
+                      postedBy:req.user._id}
+    Post.findByIdAndUpdate(req.body.postId,
+         {
+              $push: { comments: comment } 
+            }, {
+        new: true
+    }).populate("comments.postedBy","_id name").populate("postedBy", "_id name").exec((err, result) => {
+        if (err) {
+            return res.status(422).json({ error: err })
+        }
+        else {
+            res.status(200).json( result )
+        }
+    })
+})
+
+
+router.delete('/deletemypost/:postId',requireLogin, (req, res) => {
+    Post.findOne({ _id:req.params.postId }).populate("postedBy", "_id").exec((err,post)=>{
+        if(err||!post){
+           return res.status(422).json({error:"Error in deleting post"})
+        }
+        if(post.postedBy._id.toString()=== req.user._id.toString()){
+      post.remove().then(result=>{
+          res.status(200).json(result)
+      }).catch(err=>{
+        console.log(err)
+    })
+        }
+    
+    })
+})
+
 module.exports = router
